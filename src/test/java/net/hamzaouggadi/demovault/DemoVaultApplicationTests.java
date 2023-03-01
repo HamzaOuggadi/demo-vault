b55109c832585dc4062c5af211f4a1830d522902
Hamza OUGGADI <hamza.ouggadi@atos.net>
2023-03-01 16:19:32 +0100

    Initial commit

---
+++ src/test/java/net/hamzaouggadi/demovault/DemoVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hamzaouggadi.demovault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
